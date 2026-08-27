1791ff7e55e638b00d2ab87b9585ef3d8d8907c0
Fardeen <mfa91631@gmail.com>
2026-08-27 21:05:52 +0530

    chore: initialize Job Board API

---
+++ app/__init__.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+jwt = JWTManager()
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("config.Config")
+    db.init_app(app)
+    bcrypt.init_app(app)
+    jwt.init_app(app)
+
+    @app.route("/")
+    def home():
+        return jsonify({
+            "project": "Job Board & Mini ATS API",
+            "status": "Running",
+            "version": "1.0"
+        })
+
+    return app
+    
---
+++ tests/test_app.py
@@ -0,0 +1,9 @@
+from app import create_app
+
+def test_home_route():
+    app = create_app()
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
