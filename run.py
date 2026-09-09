a979caa57a054d879fbe552a86f67040407463ce
Fardeen <mfa91631@gmail.com>
2026-09-09 15:57:01 +0530

    Prepare Flask app for deployment

--- run.py
+++ run.py
@@ -6,4 +6,10 @@ with app.app_context():
     db.create_all()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    
+    app.run(
+        host="0.0.0.0",
+        port=int(os.getenv("PORT",5000)),
+        debug=False
+        )
